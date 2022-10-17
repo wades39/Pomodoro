380b77f1301358f99f3ee9d05764928ab38587f7
kyler <kyler@10.0.0.24>
2022-10-16 22:06:24 -0600

    Defined an interface for graphical timers
    This interface outlines all of the methods needed for a graphical timer.

---
+++ src/graphics/GraphicalTimer.java
@@ -0,0 +1,49 @@
+package graphics;
+
+/**
+ * An interface that defines how graphical timers will function.
+ * 
+ * @author wades39
+ *
+ */
+public interface GraphicalTimer {
+
+	/**
+	 * Performs a tick, updating the time remaining, and triggers a new render
+	 * event.
+	 * 
+	 * @param timeRemaining - The time left in this stage, used in rendering the
+	 *                      graphics.
+	 */
+	public void tick(long timeRemaining);
+
+	/**
+	 * Sets the target time for the graphical timer from which the graphics will be
+	 * rendered.
+	 * 
+	 * @param newTargetTime - The new target time from which the graphics will be
+	 *                      rendered.
+	 * @return True if a change was made, false otherwise.
+	 */
+	public boolean setTargetTime(long newTargetTime);
+
+	/**
+	 * Sets the stage text for the graphical timer. This text tells the user what
+	 * stage they are in.
+	 * 
+	 * @param newStageText - The text used to indicate the stage the user is in.
+	 * @return True if a change was made, false otherwise.
+	 */
+	public boolean setStageText(String newStageText);
+
+	/**
+	 * Allows for the setting of colors to use when rendering the timer.
+	 * 
+	 * @param hexColorStrings - A comma-separated list of hex color codes in string
+	 *                        format: e.g.
+	 *                        setColorTheme("#FFFFFF","#000000","#7F7F7F")
+	 * @return True if a change is made, false otherwise.
+	 */
+	public boolean setColorTheme(String... hexColorStrings);
+
+}
