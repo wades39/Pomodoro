f433997ec28aaa45a3c77e08f7f450bc127d1c24
kyler <kyler@10.0.0.24>
2022-10-16 21:09:31 -0600

    Begin defining preferences
    Began defining preferences that the user can change while setting up the
    application.

--- src/function/Preferences.java
+++ src/function/Preferences.java
@@ -2,9 +2,33 @@ package function;
 
 /**
  * Houses the user's preferences in a way that are accessible to other classes.
+ * 
  * @author wades39
  *
  */
 public class Preferences {
 
+	/* --| PREFERENCE VARIABLES |-- */
+
+	/**
+	 * Indicates whether or not to force breaks by maximizing the window and
+	 * preventing window movement during breaks.
+	 */
+	private boolean FORCE_BREAKS;
+
+	/**
+	 * Indicates whether or not to render the application in windowed mode or
+	 * undecorated.
+	 * 
+	 * True == render with frame decorations, false == undecorated.
+	 */
+	private boolean RENDER_AS_WINDOWED;
+
+	/**
+	 * Indicates whether or not to use custom timing for the Pomodoro timer.
+	 * 
+	 * Custom timing means timing is set differently than the standard Pomodoro
+	 * Technique times.
+	 */
+	private boolean USE_CUSTOM_TIMING;
 }
