dfa1b301155db7f7b29b1d74a22884254912c203
kyler <kyler@10.0.0.24>
2022-10-26 20:05:14 -0600

    Added header tooltip label
    Added a label to the dialog that displays the purpose of the dialog.

--- src/graphics/PreferencesSelectorDialog.java
+++ src/graphics/PreferencesSelectorDialog.java
@@ -4,7 +4,10 @@ import java.awt.Dialog;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.border.EmptyBorder;
 
 /**
  * A window that allows the user to modify their preferences for the
@@ -17,6 +20,7 @@ public class PreferencesSelectorDialog implements ComponentListener {
 
 	/* --| CONSTANTS |-- */
 	private static final String PREFERENCES_SELECTOR_WINDOW_TITLE = "Pomodoro Preferences";
+	private static final String MODIFY_PREFERENCES_DIALOG_HEADER_TEXT = "<html>In this window, you're able to modify your preferences.<br>These preferences adjust how the application works.</html>";
 
 	/* --| VARIABLES |-- */
 
@@ -36,20 +40,26 @@ public class PreferencesSelectorDialog implements ComponentListener {
 
 		// boilerplate window setup
 		frame = new JDialog(parent, PREFERENCES_SELECTOR_WINDOW_TITLE, Dialog.ModalityType.APPLICATION_MODAL);
-
+		frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		
 		// handle closing the window
 		frame.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
 		frame.addComponentListener(this);
 
-		// TODO : render listings for all preference options
+		// declare and add the header panel to the dialog
+		JLabel headerLabel = new JLabel(MODIFY_PREFERENCES_DIALOG_HEADER_TEXT);
+		headerLabel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		frame.add(headerLabel);
 
+		// TODO : add components to populate the dialog
+		
 		// show the window
 		frame.pack();
 		frame.setVisible(true);
 	}
 
-	/*	--| INTERFACE OVERRIDES |--	*/
-	
+	/* --| INTERFACE OVERRIDES |-- */
+
 	@Override
 	public void componentResized(ComponentEvent e) {
 		// do nothing
